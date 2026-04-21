559c9df6fb8c1a65210a497fcfb0b5f2aff0a227
Harsh Verma <vermaharshsoftwaredeveloper@gmail.com>
2026-04-21 12:35:45 +0530

    added new origin

--- src/app.js
+++ src/app.js
@@ -22,6 +22,8 @@ const allowedOrigins = [
 	"https://zoykah.com",
 	"https://www.zoykah.com",
 	"https://zoykah-dashboard.vercel.app",
+	"https://dashboard.zoykah.com",
+	"https://www.dashboard.zoykah.com"
 ];
 
 if (NODE_ENV !== "production") {
